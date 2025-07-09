ed10950a1bdd7ef4aa72335e26ca47197483638a
Livia Galletti <liligalletti@gmail.com>
2025-07-09 16:54:44 -0300

    hello world delay

--- Hello_World.py
+++ Hello_World.py
@@ -1,5 +1,20 @@
+
+import time
+
+def slow_print(text, delay=0.05):
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+    print()
+
 def main():
-    print("Hello World")
+    frame_top = "+" + "-" * 24 + "+"
+    message = "|   Hello, World! :)   |"
+    frame_bottom = "+" + "-" * 24 + "+"
+
+    print(frame_top)
+    slow_print(message)
+    print(frame_bottom)
 
 if __name__ == "__main__":
     main()
